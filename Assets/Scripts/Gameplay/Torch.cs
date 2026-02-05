6b35631845ce421723dc748b1131606a656dc154
Luc Sarrazin <luc.sarrazin@free.fr>
2026-02-05 17:05:24 +0100

    Chest Matches, and torches

---
+++ Assets/Scripts/Gameplay/Torch.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    private bool oneTime = false;
+    [SerializeField] private Material _colorOff;
+    [SerializeField] private Material _colorOn;
+    [SerializeField] private GameObject _pointLight;
+    private MeshRenderer _meshRenderer;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer.material =  _colorOff;
+        _pointLight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Matches touch the torch");
+        if (other.CompareTag("Matches"))
+        {
+            if (!oneTime)
+            {
+                _pointLight.SetActive(true);
+                _meshRenderer.material = _colorOn;
+                oneTime = true;
+            }
+        }
+    }
+}
