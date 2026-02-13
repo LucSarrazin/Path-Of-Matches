c5b5d979ff4570bb9dfcc3a596f1c4dde1a69c67
Lytheaa <pauline.pace97@gmail.com>
2026-02-13 19:24:40 +0100

    Debut UI

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _matches;
+    [SerializeField] private TextMeshProUGUI _bpmCount;
+    [SerializeField] private Image _pointer;
+
+}
