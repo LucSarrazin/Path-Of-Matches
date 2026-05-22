07efba9fa0e839e4ac34bcca7d30861cc5a56de4
Lytheaa <pauline.pace97@gmail.com>
2026-05-22 17:47:23 +0200

    Add Save System to MainScene

--- Assets/Scripts/SaveSystem/SaveData.cs
+++ Assets/Scripts/SaveSystem/SaveData.cs
@@ -1,11 +1,11 @@
 [System.Serializable]
 public class SaveData
 {
-    public float _targetPosX = 0f; 
-    public float _targetPosY = 2f;
-    public float _targetPosZ = 0f;
+    public float _targetPosX = 24f; 
+    public float _targetPosY = -5f;
+    public float _targetPosZ = -48f;
 
     public int _matchesCount = 5;
 
-    public float _pointerSensitivity = 0.17f;
+    public float _pointerSensitivity = 0.2f;
 }
