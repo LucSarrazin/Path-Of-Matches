713695db29b913bd12099f694e877bcb3cf55c23
Lytheaa <pauline.pace97@gmail.com>
2026-05-26 22:21:49 +0200

    Add PlayerFootStepScript and SFX Library

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -2,9 +2,5 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [Header("[SFX - PLAYER]")]
-    [Header("Footsteps : ")]
-    [SerializeField] private AudioClip _footstepsOnWood;
-    [SerializeField] private AudioClip _footstepsOnMud;
-    [SerializeField] private AudioClip _footstepsOnStone;
+
 }
--- Assets/Scripts/Player/PlayerGroundChecker.cs
+++ Assets/Scripts/Player/PlayerGroundChecker.cs
@@ -2,22 +2,25 @@ using UnityEngine;
 
 public class PlayerGroundChecker : MonoBehaviour
 {
-    [SerializeField] private float _distance;
-    [SerializeField] private LayerMask _currentGround;
+    [SerializeField] private float _checkDistance = 0.5f;
+    private LayerMask _layerMask;
 
-    private int GetCurrentGroundLayer()
+    public int GetGroundLayer()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _distance, _currentGround);
-        if (hit.collider != null)
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector2.down, out hit, _checkDistance, _layerMask))
         {
             return hit.collider.gameObject.layer;
         }
         return -1;
     }
 
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, Vector2.down * _distance);
+        Gizmos.DrawRay(transform.position, Vector2.down * _checkDistance);
     }
+
 }
