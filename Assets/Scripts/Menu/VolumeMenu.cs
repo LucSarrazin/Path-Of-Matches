f52f4775e1d3f8836ffa6723f1663b996a638c65
Lytheaa <pauline.pace97@gmail.com>
2026-06-02 22:15:44 +0200

    [SOUND MENU] Fix bug Luc

--- Assets/Scripts/Menu/VolumeMenu.cs
+++ Assets/Scripts/Menu/VolumeMenu.cs
@@ -18,44 +18,51 @@ public class VolumeMenu : MonoBehaviour
         }
         else
         {
-            SetAmbianceVolume();
-            SetSFXVolume();
-            SetMasterVolume();
+            SetMasterVolume(volumeMasterSlider.value);
+            SetAmbianceVolume(volumeAmbianceSlider.value);
+            SetSFXVolume(volumeSFXSlider.value);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SetAmbianceVolume(float value)
     {
-        
+        float volume = Mathf.Log10(value) * 20;
+        mixer.SetFloat("AMBIANCE", volume);
+        PlayerPrefs.SetFloat("AmbianceVolume", value);
+
     }
 
-    public void SetAmbianceVolume()
+
+    public void SetSFXVolume(float value)
     {
-        float volume = volumeAmbianceSlider.value;
-        mixer.SetFloat("AMBIANCE", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("AmbianceVolume", volume);
+        float volume = Mathf.Log10(value) * 20;
+        mixer.SetFloat("SFX", volume);
+        PlayerPrefs.SetFloat("SFXVolume", value);
     }
-    public void SetSFXVolume()
+
+    public void SetMasterVolume(float value)
     {
-        float volume = volumeSFXSlider.value;
-        mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("SFXVolume", volume);
+        float volume = Mathf.Log10(value) * 20;
+        mixer.SetFloat("MASTER", volume);
+        PlayerPrefs.SetFloat("MasterVolume", value);
     }
-    public void SetMasterVolume()
-    {
-        float volume = volumeSFXSlider.value;
-        mixer.SetFloat("MASTER", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("MASTERVolume", volume);
-    }
+
 
     public void LoadVolume()
     {
-        volumeAmbianceSlider.value = PlayerPrefs.GetFloat("AmbianceVolume");
-        volumeSFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
-        volumeMasterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
-        SetAmbianceVolume();
-        SetSFXVolume();
-        SetMasterVolume();
+        float masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+        float ambianceVolume = PlayerPrefs.GetFloat("AmbianceVolume");
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+
+        volumeMasterSlider.value = masterVolume;
+        volumeAmbianceSlider.value = ambianceVolume;
+        volumeSFXSlider.value = sfxVolume;
+
+        SetAmbianceVolume(ambianceVolume);
+        SetSFXVolume(sfxVolume);
+        SetMasterVolume(masterVolume);
+
+        //Debug.Log($"Load volumes : MASTER {masterVolume} | AMBIANCE {ambianceVolume} | SFX {sfxVolume}");
     }
 }
