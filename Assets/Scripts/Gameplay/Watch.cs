2139e19c225ba0701fb5555ab7d9a8b326d1d170
whiezzer <endyturrin@gmail.com>
2026-06-05 16:13:34 +0200

    (Fix) hands animations

--- Assets/Scripts/Gameplay/Watch.cs
+++ Assets/Scripts/Gameplay/Watch.cs
@@ -1,13 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class Watch : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     private bool switched = false;
+    private bool isTakingMatches = false;
 
     // Ajout un peu beaucoup schlag pour tester, a revoir avec Luc pour modif de son script 
     //Juste pour patch rapide dans PlayerInteractState( -> Ranger montre en mode inspection) 
     public void SetBoolSwitch(bool value) { switched = value; }
+    public bool GetBoolTakeMatches() { return isTakingMatches; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,17 +26,40 @@ public class Watch : MonoBehaviour
 
     public void openWatch()
     {
+        if (!isTakingMatches)
+        {
+            if (!switched)
+            {
+                switched = true;
+                Debug.Log("Close Watch");
+                animator.SetBool("Close", true);
+            }
+            else
+            {
+                switched = false;
+                Debug.Log("Open Watch");
+                animator.SetBool("Close", false);
+            }
+        }
+    }
+
+    public IEnumerator waitAnimTakeMatches()
+    {
         if (!switched)
         {
-            switched = true;
             Debug.Log("Close Watch");
             animator.SetBool("Close", true);
+            isTakingMatches = true;
+            yield return new WaitForSeconds(2);
+            Debug.Log("Open Watch");
+            animator.SetBool("Close", false);
+            isTakingMatches = false;
         }
         else
         {
-            switched = false;
-            Debug.Log("Open Watch");
-            animator.SetBool("Close", false);
+            isTakingMatches = true;
+            yield return new WaitForSeconds(2);
+            isTakingMatches = false;
         }
     }
 }
