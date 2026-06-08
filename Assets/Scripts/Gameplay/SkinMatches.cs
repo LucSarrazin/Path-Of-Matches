21539cfb141cdf06a480f472bb5d050317cdb5c2
Luc Sarrazin <luc.sarrazin@free.fr>
2026-06-08 20:17:49 +0200

    Change script and terrain

--- Assets/Scripts/Gameplay/SkinMatches.cs
+++ Assets/Scripts/Gameplay/SkinMatches.cs
@@ -25,6 +25,7 @@ public class SkinMatches : Interactable
         oneTime = true;
         // -- Adds the skin to the player's skin list when they interact with -- //
         _playerReferences.PlayerSwitchMatches.AddMatchesSkin(skin);
+        _playerReferences.PlayerLaunchMatches.NumberOfMatches = 10;
 
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<Collider>().enabled = false;
