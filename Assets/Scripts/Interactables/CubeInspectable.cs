80377f61ed9bfcedd2fe090266a34778f6395050
Lytheaa <pauline.pace97@gmail.com>
2026-06-19 18:44:31 +0200

    [SFX] + Coin / Book / Bottle

--- Assets/Scripts/Interactables/CubeInspectable.cs
+++ Assets/Scripts/Interactables/CubeInspectable.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class CubeInspectable : Inspectable
 {
+    [SerializeField] AudioSource _audioSrc;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _audioSrc = GetComponent<AudioSource>();
+    }
+
     public override void Interact()
     {
         base.Interact();
+        _audioSrc.Play();
+        
         /* Here : inspectable special methods for this object */
     }
 }
