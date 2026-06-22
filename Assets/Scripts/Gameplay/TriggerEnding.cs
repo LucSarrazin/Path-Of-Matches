d3f0ef3f83596edde85d90e7af60b5e1ade52ecd
Lytheaa <pauline.pace97@gmail.com>
2026-06-23 00:31:35 +0200

    Trigger ending

--- Assets/Scripts/Gameplay/TriggerEnding.cs
+++ Assets/Scripts/Gameplay/TriggerEnding.cs
@@ -1,21 +1,27 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerEnding : MonoBehaviour
 {
     private bool oneTime = false;
     [SerializeField] private Animator blinkingEyes;
     [SerializeField] private PlayerReferences _playerReferences;
+    [SerializeField] private GameObject _endingCanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (_endingCanvas.activeSelf)
+        {
+            _endingCanvas.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,8 +35,19 @@ public class TriggerEnding : MonoBehaviour
                 _playerReferences.PlayerMovements.CanMove(false);
                 _playerReferences.PlayerMovements.CanLook(false);
 
-                GameEvents.OnDeleteSaveRequested?.Invoke();
+                StartCoroutine(PlayEndScene());
+
             }
         }
+    }
+
+    private IEnumerator PlayEndScene()
+    {
+        yield return new WaitForSeconds(3f);
+        _endingCanvas.SetActive(true);
+        yield return new WaitForSeconds(6f);
+        GameEvents.OnDeleteSaveRequested?.Invoke();
+
+        SceneManager.LoadScene(0);
     }
 }
