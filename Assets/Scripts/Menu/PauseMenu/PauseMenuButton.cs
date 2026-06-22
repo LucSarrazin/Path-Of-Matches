de7dc4ed938dc3ffcb38645ac016a39ad4dcca09
Lytheaa <pauline.pace97@gmail.com>
2026-06-22 20:37:59 +0200

    Fix UI InGame Menu :')

--- Assets/Scripts/Menu/PauseMenu/PauseMenuButton.cs
+++ Assets/Scripts/Menu/PauseMenu/PauseMenuButton.cs
@@ -10,12 +10,19 @@ public class PauseMenuButton : MonoBehaviour
 
     public void ReturnToMenu()
     {
+        Debug.Log("Try to go back to menu");
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
     
     public void Quit()
     {
+        Debug.Log("Try to quit app");
         Application.Quit();
+    }
+
+    public void Test()
+    {
+        Debug.Log("Try test button click");
     }
 }
